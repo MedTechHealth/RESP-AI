7192cb3bc3e7fd8063b46713ae8f802c6a819b65
Joel <joelshibuadoor@gmail.com>
2026-03-28 20:56:37 +0530

    refactor: Use relative imports for GlassmorphicContainer

--- app/test/widgets/glassmorphic_container_test.dart
+++ app/test/widgets/glassmorphic_container_test.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:app/widgets/glassmorphic_container.dart';
+import '../../lib/widgets/glassmorphic_container.dart';
 import 'dart:ui';
-import 'package:app/theme/app_theme.dart';
+import '../../lib/theme/app_theme.dart';
 
 void main() {
   group('GlassmorphicContainer', () {
